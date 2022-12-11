faec15adf35a0cc9d310250a61b1a9a0d4a69572
Andrey Gavrilov <boombardir21@gmail.com>
2022-12-11 13:27:06 +0500

    4

--- index.js
+++ index.js
@@ -32,3 +32,44 @@
 //
 // console.log(tesla.rating)
 
+class Car {
+  constructor(brand) {
+    this.brand = brand
+    this.gasTank = 100
+    this.zapravka = []
+  }
+
+  getGas(){
+    this.gasTank += 10
+    const stamp = Date.now()
+    const time = new Date(stamp)
+    this.zapravka.push(time.toString())
+    return this.gasTank
+  }
+
+  drive(){
+    this.gasTank -= 10
+    return this.gasTank
+  }
+}
+
+class HybridCar extends Car {
+  constructor(brand, model) {
+    super(brand); //Car(brand)
+    this.model = model
+  }
+
+  autoPark(){
+    console.log('autoPark')
+  }
+
+}
+
+const nissan = new Car('Nissan')
+const lexus = new HybridCar('lexus', 'LX')
+
+nissan.drive()
+nissan.getGas()
+
+console.log(nissan)
+console.log(lexus.autoPark())
